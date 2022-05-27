697380f8d1257ad4b64c3d757f7079d34ee69f0a
Ricardo Moura <ricardo.moura@liferay.com>
2022-05-27 11:50:14 -0300

    Customize port, context path and app name

--- src/main/java/com/odrakir1/SalesApplication.java
+++ src/main/java/com/odrakir1/SalesApplication.java
@@ -5,43 +5,57 @@ import com.odrakir1.domain.entity.Order;
 import com.odrakir1.domain.repository.Customers;
 import com.odrakir1.domain.repository.Orders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SalesApplication {
 
+
     @Bean
-    public CommandLineRunner init(@Autowired Customers customers, @Autowired Orders orders){
-      return args -> {
-          Customer newCustomer = new Customer();
-          newCustomer.setName("Linus");
-          customers.save(newCustomer);
+    public CommandLineRunner init(@Autowired Customers customers, @Autowired Orders orders) {
+        return args -> {
+            Customer newCustomer = new Customer();
+            newCustomer.setName("Linus");
+            customers.save(newCustomer);
 
-          Order order = new Order();
-          order.setCustomer(newCustomer);
-          order.setOrdersDate(LocalDate.now());
-          order.setTotal(BigDecimal.valueOf(1234.99));
+            Order order = new Order();
+            order.setCustomer(newCustomer);
+            order.setOrdersDate(LocalDate.now());
+            order.setTotal(BigDecimal.valueOf(1234.99));
 
-          orders.save(order);
+            orders.save(order);
 
-          Customer customer = customers.findCustomerFetchOrders(newCustomer.getId());
-          System.out.println(customer);
-          System.out.println(customer.getOrders());
+            Customer customer = customers.findCustomerFetchOrders(newCustomer.getId());
+            System.out.println(customer);
+            System.out.println(customer.getOrders());
 
-          orders.findByCustomer(newCustomer).forEach(System.out::println);
-      };
+            orders.findByCustomer(newCustomer).forEach(System.out::println);
+        };
 
     }
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${server.port}")
+    private String port;
 
     public static void main(String[] args) {
         SpringApplication.run(SalesApplication.class, args);
     }
 
+    @GetMapping("/application")
+    public String application() {
+        return applicationName + " running on port: " + port;
+    }
 }
